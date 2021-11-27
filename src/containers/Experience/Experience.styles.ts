d00d5965c1921bcf87753774103687049e75f3d6
nguyenvantruong9902 <nguyenvantruong9902@gmail.com>
2021-11-27 12:32:03 +0700

    add icon for each timeline

--- src/containers/Experience/Experience.styles.ts
+++ src/containers/Experience/Experience.styles.ts
@@ -2,6 +2,6 @@ import styled from '@emotion/styled';
 
 export const WrapperExperience = styled.div`
   .vertical-timeline-element-icon {
-    box-shadow: none;
+    box-shadow: 0 0 0 4px rgba(0, 0, 0, 0.5) inset 0 2px 0 rgb(0, 0, 0.8);
   }
 `;
--- src/containers/Experience/index.tsx
+++ src/containers/Experience/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FcGraduationCap } from 'react-icons/fc';
+import { MdOutlineDeveloperMode } from 'react-icons/md';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 
-import { timeLines } from './timelines';
 import { WrapperExperience } from './Experience.styles';
 
 import 'react-vertical-timeline-component/style.min.css';
+
+export const timeLines = [
+  {
+    date: '09/2017 - 12/2019',
+    title: 'Student',
+    work: 'Learn Information Technology (Software Application)',
+    workLocation: 'FPT Polytechnic College',
+    icon: <FcGraduationCap />,
+  },
+  {
+    date: '10/2020 - Present',
+    title: 'Developer ReactJS',
+    work: 'Developer, Learn how to work, Learn ReactNative',
+    workLocation: 'HD WEBSOFT, BeautyBooking Team (Start Up)',
+    icon: <MdOutlineDeveloperMode />,
+  },
+];
+
 interface ExperienceProps {
   visible?: boolean;
 }
@@ -20,6 +39,7 @@ export const Experience: React.FC<ExperienceProps> = ({ visible }) => {
             contentArrowStyle={{ borderRight: '7px solid  rgba(0, 0, 0, 0.5)' }}
             date={timeLine.date}
             iconStyle={{ background: 'white' }}
+            icon={timeLine.icon}
           >
             <h3>{timeLine.title}</h3>
             <h5>{timeLine.workLocation}</h5>
